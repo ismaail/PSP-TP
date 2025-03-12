4f2793f8d4cff2a084e0373d4e4c36abb5a19bf8
ismaail <contact@ismaail.com>
2025-03-12 01:05:10 +0000

    Add migration to create payouts table

---
+++ database/migrations/2025_03_11_184323_create_payouts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('bank_name');
+            $table->string('bank_country', 2);
+            $table->string('iban');
+            $table->string('bic');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+};
